c0343960d93ae4fb589eed5cf388f3cbd93b21db
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-28 18:09:24 +0800

    modify nx.sh

--- vimremotecopy/nx.sh
+++ vimremotecopy/nx.sh
@@ -1,4 +1,19 @@
+#! /bin/sh
+
+copyboard=""
+
+if [[ `uname` == 'Darwin' ]]; then
+	copyboard="pbcopy"
+    echo "Mac OS"
+fi
+
+
+if [[ `uname` == 'Linux' ]]; then
+	copyboard="xclip"
+    echo "Linux"
+fi
+
 while :
 do
-	nc -l 8377 | xclip -selection c
+	nc -l 8377 | $copyboard -selection c
 done
